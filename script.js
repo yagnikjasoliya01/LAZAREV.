819ae882bedf079d9647f39e5ce3e739a3cd590e
Yagnik Jasoliya <146578487+yagnikjasoliya01@users.noreply.github.com>
2024-07-24 00:57:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,70 @@
 function navAnimation() {
 
-var nav = document.querySelector("nav");
-nav.addEventListener("mouseenter", function () {
-    let tl = gsap.timeline()
+    var nav = document.querySelector("nav");
+    nav.addEventListener("mouseenter", function () {
+        let tl = gsap.timeline()
 
-    tl.to("#nav-bottom", {
-        height: "21vh",
-        duration: 0.5
-    })
-    tl.to(".nav-part2 h5", {
-        display: "block",
-        duration: 0.1
+        tl.to("#nav-bottom", {
+            height: "21vh",
+            duration: 0.5
+        })
+        tl.to(".nav-part2 h5", {
+            display: "block",
+            duration: 0.1
 
+        })
+        tl.to(".nav-part2 h5 span", {
+            y: 0,
+            // duration:0.3,
+            stagger: {
+                amount: 0.3
+            }
+        })
     })
-    tl.to(".nav-part2 h5 span", {
-        y: 0,
-        // duration:0.3,
-        stagger: {
-            amount: 0.3
-        }
+    nav.addEventListener("mouseleave", function () {
+        let tl = gsap.timeline()
+        tl.to(".nav-part2 h5 span", {
+            y: 25,
+            stagger: {
+                amount: 0.155
+            }
+        })
+        tl.to(".nav-part2 h5", {
+            display: "none",
+            duration: 0.1
+        })
+        tl.to("#nav-bottom", {
+            height: 0,
+            duration: 0.4
+        })
     })
-})
-nav.addEventListener("mouseleave", function () {
-    let tl = gsap.timeline()
-    tl.to(".nav-part2 h5 span", {
-        y: 25,
-        stagger: {
-            amount: 0.155
-        }
-    })
-    tl.to(".nav-part2 h5", {
-        display: "none",
-        duration: 0.1
-    })
-    tl.to("#nav-bottom", {
-        height: 0,
-        duration: 0.4
-    })
-})
 }
-
 navAnimation()
+
+function page2Animation() {
+    var rightElems = document.querySelectorAll(".right-elem");
+
+    rightElems.forEach(function (elem) {
+        elem.addEventListener("mouseenter", function () {
+            gsap.to(elem.childNodes[3], {
+                opacity: 1,
+                scale: 1
+            })
+        })
+        elem.addEventListener("mouseleave", function () {
+            gsap.to(elem.childNodes[3], {
+                opacity: 0,
+                scale: 0
+            })
+        })
+        elem.addEventListener("mousemove", function (dets) {
+            gsap.to(elem.childNodes[3], {
+                x: dets.x - elem.getBoundingClientRect().x - 60,
+                y: dets.y - elem.getBoundingClientRect().y - 100
+            })
+        })
+    })
+}
+page2Animation();
+
 
